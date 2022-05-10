2ce847125dce8873742239751780af90d25210f5
dameeolawuyi <83360411+dameeolawuyi@users.noreply.github.com>
2022-05-10 02:51:21 +0100

    Create flashloantest.js

---
+++ flashloantest.js
@@ -0,0 +1,11 @@
+const flashloan = artifacts.require(“flashloan”);
+ contract("flashloan", (accounts) => {
+   it("should be able to borrow Ethers", () => {
+   })
+ })
+
+let [addr x, addr y ]= addresses;
+    it("should be able to create a new loan", async () => {
+    })
+})
+
