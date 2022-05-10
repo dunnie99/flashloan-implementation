59b2473d4f37cc5925f26af3ffb3c585b46ad903
dameeolawuyi <83360411+dameeolawuyi@users.noreply.github.com>
2022-05-10 03:52:58 +0100

    Update flashloantest.js

--- flashloantest.js
+++ flashloantest.js
@@ -6,8 +6,7 @@ const flashloan = artifacts.require(“flashloan”);
 
 let [addr x, addr y ]= addresses;
     it("should be able to create a new loan", async () => {
-    })
-})
+    
 
 const contractInstance = await flashloancontract.new();
 const tokenName = ["loan 1];
@@ -17,3 +16,5 @@ contractInstance.flashLoan(tokenName[0]);
  assert.equal(result.receipt.status, true);
  assert.equal(result.logs[0].args.addr,tokenName[0]);
 
+await contractInstance.flashLoan([0], {from: lendingpool});
+await utils.shouldThrow(contractInstance.flashLoan(tokenName[1], {from: lendingpool}));
