b822566ac125bae2f1f1bdd26f9fd0abd6856c0e
dameeolawuyi <83360411+dameeolawuyi@users.noreply.github.com>
2022-05-12 18:56:30 +0100

    Update flashloantest.js

--- flashloantest.js
+++ flashloantest.js
@@ -1,19 +1,35 @@
-const flashloan = artifacts.require(“flashloan”);
- contract("flashloan", (accounts) => {
-   it("should be able to borrow Ethers", () => {
-   
+// SPDX-License-Identifier: GPL-3.0
+        
+pragma solidity >=0.4.22 <0.9.0;
 
-let [addr x, addr y ]= ILendingPoolAddresses;
-    it("should be able to create a new loan", async () => {
+// This import is automatically injected by Remix
+import "remix_tests.sol"; 
+import "remix_accounts.sol";
+import "../Myflashloancontract.sol";
+
+contract testSuite {
+    MyFlashloanContract foo;
+
+    /// 'beforeEach' runs before EACH tests
+    function beforeEach() public {
+            foo=  new MyFlashloanContract();
+    }
     
+    function testItBorrowedToken() public {
+         uint amount = 1 ether;
+         address asset = address(0x428D4b75c90eF87d6c586D9423a07c3a7E0a968A);
+         Assert.equal(foo.flashloan(), 1, "loan not recieved");
+  }
 
-const contractInstance = await flashloancontract.new();
-const tokenName = ["loan 1];
-contractInstance.flashLoan(tokenName[0]);
+    function getContractBalance() public {
+        address _reserve;
+        uint256 _amount;
+        uint256 _fee;
+        bytes memory _params;
 
- const result = await contractInstance.flashLoan (tokenName[0], {from: lendingPool});
- assert.equal(result.receipt.status, true);
- assert.equal(result.logs[0].args.addr,tokenName[0]);
+         require(_amount <= (address(this), _reserve), "Invalid balance, was the flashLoan successful?");
+        Assert.greaterThan(_amount, address(this), "2 should be greater than to 1");
 
-await contractInstance.flashLoan([0], {from: lendingpool});
-await utils.shouldThrow(contractInstance.flashLoan(tokenName[1], {from: lendingpool}));
+    }
+
+}
