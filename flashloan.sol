0666f268d4fcbdb87212d60825101dba882934fc
Amarachi <49257104+amarachiugwu@users.noreply.github.com>
2022-05-14 02:04:38 +0100

    added exchange

--- flashloan.sol
+++ flashloan.sol
@@ -3,9 +3,19 @@ pragma solidity ^0.6.6;
 import "./FlashLoanReceiverBase.sol";
 import "./ILendingPoolAddressesProvider.sol";
 import "./ILendingPool.sol";
+import "../exchange/Exchange.sol";
 
-contract FlashloanV1 is FlashLoanReceiverBaseV1 {
-    //_addressProvider = address of one of the Lending Pool Providers of Aave
+contract FlashloanV1 is FlashLoanReceiverBaseV1, Exchange {
+    address _reserve = address(this);
+    /**  
+        _addressProvider = address of one of the Lending Pool Providers of Aave
+
+        using DAI LendingPool address 0x506B0B2CF20FAA8f38a4E2B524EE43e1f4458Cc5 
+
+        A list of all deployed contract addresses can be found athttps://docs.aave.com/developers/v/1.0/deployed-contracts/deployed-contract-instances
+
+    */
+
     constructor(address _addressProvider) FlashLoanReceiverBaseV1(_addressProvider) public{}
 
     /**
@@ -37,6 +47,32 @@ contract FlashloanV1 is FlashLoanReceiverBaseV1 {
         // Your logic goes here.
         // !! Ensure that *this contract* has enough of `_reserve` funds to payback the `_fee` !!
         //
+
+        address token1 = 0xFf795577d9AC8bD7D90Ee22b6C1703490b6512FD;
+        address token2 = 0x13512979ADE267AB5100878E2e0f485B568328a4;
+        // address ownerOfToken1 = 0x2853eB1d81342453c6fA7FF7D03C97a8F142EabA;
+        address ownerOfToken1 = address(this);
+        address ownerOfToken2 = 0x3ceEE3CF67314501Ec960AC81E7e5A58b99Dbb7a;
+        uint amountOfToken1 = 1 ether;
+        uint amountOfToken2 = 1 ether;
+
+        swap(
+            token1,
+            token2,
+            ownerOfToken1,
+            ownerOfToken2,
+            amountOfToken1,
+            amountOfToken2
+        );
+
+        swap(
+            token2,
+            token1,
+            ownerOfToken2,
+            ownerOfToken1,
+            amountOfToken2,
+            amountOfToken1
+        );
 
         uint totalDebt = _amount.add(_fee);
         transferFundsBackToPoolInternal(_reserve, totalDebt);
